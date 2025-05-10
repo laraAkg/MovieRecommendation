8dd751581aa4efcc57d3af245d9ff48876ff8a01
zakgul-bbc <lara.akguen@gmail.com>
2025-05-10 12:09:45 +0200

    Add CSV files to .gitignore to prevent tracking of dataset artifacts

--- src/main/data_processing/clean_dataset.py
+++ src/main/data_processing/clean_dataset.py
@@ -6,32 +6,25 @@ import os
 
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Configure logger
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
-# Load data
 logger.info("Loading data...")
 df = load_data(max_missing=3)
 
-# Filter important columns
 logger.info("Filtering important columns...")
 df = filter_important_columns(df)
 
-# Clean data
 logger.info("Cleaning data...")
 
-# Extract names from production_countries and production_companies
 df.dropna(subset=['title'], inplace=True)
 df = df.drop_duplicates(subset='title', inplace=False)
 
 
-# Save data to MongoDB
 logger.info("Saving data to MongoDB...")
 mongo_handler = MongoDBHandler(
     os.getenv("MONGO_URI"), os.getenv("DB_NAME"), os.getenv("COLLECTION_NAME")
